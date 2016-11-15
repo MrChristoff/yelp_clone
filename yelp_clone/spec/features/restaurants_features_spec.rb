d8c1c1357b6963053bdc6b873376733d6fee2185
Katherine Pobee-Norris <kathwath@hotmail.com>
2016-11-15 11:10:16 +0000

    set up model and migrated db

--- yelp_clone/spec/features/restaurants_features_spec.rb
+++ yelp_clone/spec/features/restaurants_features_spec.rb
@@ -2,6 +2,17 @@ require 'rails_helper'
 
 feature 'restaurants' do
   context 'no restaurants have been added' do
+
+    before do
+      Restaurant.create(name: 'KFC')
+    end
+
+    scenario 'display restaurants' do
+      visit '/restaurants'
+      expect(page).to have_content('KFC')
+      expect(page).not_to have_content('No restaurants yet')
+    end
+
     scenario 'should disaplay a prompt to addd a restaurant' do
       visit '/restaurants'
       expect(page).to have_content 'No restaurants yet'
